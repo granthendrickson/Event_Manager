e2c29fd272c4ee47d38f4fa253249c75da4e9279
Grant <granthendrickson@knights.ucf.edu>
2024-04-10 14:35:13 -0400

    Map search and selection to get coordinates

--- client/src/components/CreateEvent.jsx
+++ client/src/components/CreateEvent.jsx
@@ -1,4 +1,33 @@
 import React, { useState } from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+import usePlacesAutocomplete, {
+	getGeocode,
+	getLatLng,
+} from 'use-places-autocomplete';
+import {
+	Combobox,
+	ComboboxInput,
+	ComboboxPopover,
+	ComboboxList,
+	ComboboxOption,
+} from '@reach/combobox';
+
+import '@reach/combobox/styles.css';
+
+const mapContainerStyle = {
+	width: '100%',
+	height: '500px',
+};
+const center = {
+	lat: 28.602823018995718, // default latitude
+	lng: -81.2001028153412, // default longitude
+};
+
+const options = {
+	disableDefaultUI: true,
+	zoomControl: true,
+};
 
 export default function CreateEvent() {
 	// Initialize state to store input values
@@ -15,6 +44,13 @@ export default function CreateEvent() {
 		approval_status: 'pending', // Default value
 		rso_id: '', // Default value
 		university_id: '',
+	});
+
+	const [libraries] = useState(['places']);
+
+	const { isLoaded, loadError } = useLoadScript({
+		googleMapsApiKey: 'AIzaSyBH4Ka4wzj7gy4NmfRlANFQFO3gHMPmyYk',
+		libraries,
 	});
 
 	// Function to handle input changes
@@ -41,6 +77,16 @@ export default function CreateEvent() {
 			console.error('Error creating event:', error);
 		}
 	};
+
+	const mapRef = React.useRef();
+	const onMapLoad = React.useCallback((map) => {
+		mapRef.current = map;
+	}, []);
+
+	const panTo = React.useCallback(({ lat, lng }) => {
+		mapRef.current.panTo({ lat, lng });
+		mapRef.current.setZoom(14);
+	}, []);
 
 	return (
 		<div className='createEvent'>
@@ -160,10 +206,87 @@ export default function CreateEvent() {
 							</div>
 					  ))
 					: null}
+				{loadError ? (
+					<div>Error loading maps</div>
+				) : !isLoaded ? (
+					<div>Loading maps</div>
+				) : (
+					<div>
+						<Search panTo={panTo} />
+						<GoogleMap
+							mapContainerStyle={mapContainerStyle}
+							zoom={15}
+							center={center}
+							options={options}
+							onLoad={onMapLoad}
+						>
+							<Marker position={center} />
+						</GoogleMap>
+					</div>
+				)}
 				<div className='buttonContainer'>
 					<button onClick={handleCreateEvent}>Create Event</button>
 				</div>
 			</div>
+		</div>
+	);
+}
+
+function Search({ panTo }) {
+	const {
+		ready,
+		value,
+		suggestions: { status, data },
+		setValue,
+		clearSuggestions,
+	} = usePlacesAutocomplete({
+		requestOptions: {
+			location: { lat: () => 43.6532, lng: () => -79.3832 },
+			radius: 100 * 1000,
+		},
+	});
+
+	// https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+
+	const handleInput = (e) => {
+		setValue(e.target.value);
+	};
+
+	const handleSelect = async (address) => {
+		setValue(address, false);
+		clearSuggestions();
+
+		try {
+			const results = await getGeocode({ address });
+			const { lat, lng } = await getLatLng(results[0]);
+			panTo({ lat, lng });
+			console.log({ lat, lng });
+		} catch (error) {
+			console.log('😱 Error: ', error);
+		}
+	};
+
+	return (
+		<div className='search'>
+			<Combobox onSelect={handleSelect}>
+				<ComboboxInput
+					value={value}
+					onChange={handleInput}
+					disabled={!ready}
+					placeholder='Search your location'
+				/>
+				<ComboboxPopover>
+					<ComboboxList>
+						{status === 'OK' &&
+							data.map(({ id, description }) => (
+								<ComboboxOption
+									key={id}
+									value={description}
+								/>
+							))}
+					</ComboboxList>
+				</ComboboxPopover>
+			</Combobox>
 		</div>
 	);
 }
